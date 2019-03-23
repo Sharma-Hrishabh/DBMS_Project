78e83e3e4a2a3d35ed12ff853918df32f3b8c1c8
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-03-23 18:10:18 +0530

    db connected

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,9 +1,27 @@
 from flask import Flask
+import sqlite3
+from flask import g
+
+DATABASE = '../database.db'
+
 app = Flask(__name__)
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        print('db connected')
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def hello():
+    cur = get_db().cursor()
     return "Hello World!"
 
 if __name__ == '__main__':
