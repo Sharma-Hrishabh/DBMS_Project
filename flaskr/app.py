b262b492a1c0c4b750badae6201efa92c6d9ce54
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-03-24 11:10:25 +0530

    signup form added

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,request
 import sqlite3
 from flask import g
 
@@ -30,15 +30,27 @@ c.execute('''CREATE TABLE IF NOT EXISTS users(username varchar(16), password var
 c.execute('''CREATE TABLE IF NOT EXISTS postedby (id integer, username varchar(16),
         FOREIGN KEY(id) REFERENCES blog(id), FOREIGN KEY(username) REFERENCES users(username));''')
 
-c.execute("INSERT INTO users VALUES ('hri','abc','xyz@gmail.com','Hrishabh');")
-c.execute("INSERT INTO blog VALUES ('1','qwe','qwe','dvfdvfdvfdvdfvfdv','2006-01-05');")
-c.execute('SELECT * FROM blog;')
+# c.execute("INSERT INTO users VALUES ('hri','abc','xyz@gmail.com','Hrishabh');")
+# c.execute("INSERT INTO blog VALUES ('1','qwe','qwe','dvfdvfdvfdvdfvfdv','2006-01-05');")
+# c.execute('SELECT * FROM blog;')
 
 conn.commit()
-print(c.fetchone())
+# print(c.fetchone())
 @app.route('/')
 def hello():
     return "Hello World!"
 
+@app.route('/signup/',methods=['POST', 'GET'])
+def signup():
+    if request.method=='POST':
+       username=request.form['username']
+       password=request.form['password']
+       email=request.form['email']
+       name=request.form['name']
+       print(name)
+       return "Name : "+request.method+"  "+username
+    else:
+        return render_template('signup.html')
+
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=8000,debug = True)
