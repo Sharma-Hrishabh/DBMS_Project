e68308bbf763d498d729552fc2cd009292913708
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2019-04-11 13:16:22 +0530

    tables according to schema

--- flaskr/app.py
+++ flaskr/app.py
@@ -22,19 +22,32 @@ app = Flask(__name__)
 conn = sqlite3.connect(DATABASE)
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS blog (id integer, title varchar(64), slug varchar(32),
-        body text, date text, PRIMARY KEY (id));''')
+        body text, date text, username varchar(16), PRIMARY KEY (id), 
+        FOREIGN KEY(username) REFERENCES users(username));''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS users(username varchar(16), password varchar(64),
-        email varchar(64), name varchar(32), PRIMARY KEY(username), UNIQUE(email));''')
-
-c.execute('''CREATE TABLE IF NOT EXISTS postedby (id integer, username varchar(16),
-        FOREIGN KEY(id) REFERENCES blog(id), FOREIGN KEY(username) REFERENCES users(username));''')
+        join_date text, name varchar(32), PRIMARY KEY(username));''')
+        
+c.execute('''CREATE TABLE IF NOT EXISTS email (mail varchar(64), username varchar(16),
+        FOREIGN KEY(username) REFERENCES users(username), PRIMARY KEY(mail, username));''')
+        
+c.execute('''CREATE TABLE IF NOT EXISTS wow (blog_id integer, username varchar(16), status integer,
+        FOREIGN KEY(blog_id) REFERENCES blog(id), FOREIGN KEY(username) REFERENCES users(username), 
+        PRIMARY KEY(blog_id, username));''')
+        
+c.execute('''CREATE TABLE IF NOT EXISTS comment (id integer, username varchar(16), data text,
+        blog_id integer, date text, PRIMARY KEY(id), FOREIGN KEY(blog_id) REFERENCES blog(id),
+        FOREIGN KEY(username) REFERENCES users(username));''')
+        
+c.execute('''CREATE TABLE IF NOT EXISTS category (blog_id integer, type varchar(16),
+        FOREIGN KEY(blog_id) REFERENCES blog(id), PRIMARY KEY(blog_id, type));''')
 
 # c.execute("INSERT INTO users VALUES ('hri','abc','xyz@gmail.com','Hrishabh');")
 # c.execute("INSERT INTO blog VALUES ('1','qwe','qwe','dvfdvfdvfdvdfvfdv','2006-01-05');")
 # c.execute('SELECT * FROM blog;')
 
 conn.commit()
+conn.close()
 # print(c.fetchone())
 @app.route('/')
 def hello():
@@ -52,35 +65,37 @@ def signup():
             salt = '5xy'
             actualpass=password+salt
             h = hashlib.md5(actualpass.encode())
-            cur.execute('INSERT INTO users VALUES(?, ?, ?, ?);',(username,actualpass,email,name))
+            print(h.hexdigest())
+            cur.execute('INSERT INTO users VALUES(?, ?, ?, ?);',(username,h.hexdigest(),email,name))
             con.commit()
-            # con.close()
+            con.close()
             print(name)
             return "Name : "+request.method+"  "+username
     else:
         return render_template('signup.html')
 
-@app.route('/login/')
+@app.route('/login/',methods=['POST', 'GET'])
 def login():
-    error = ''
-    if request.method == 'POST':
+    print(request.method+"")
+    if request.method=='POST':
         with sqlite3.connect(DATABASE) as c:
             cur = c.cursor()
             username = request.form['username']
             password = request.form['password']
             received_pass = password+'5xy'
             h = hashlib.md5(received_pass.encode())
+            print(h.hexdigest())
             data = cur.execute("SELECT * FROM users WHERE username = (%s)",username)
-            data = c.fetchone()[2]
-            if h == data:
+            data = c.fetchone()[1]
+            print(data)
+            if h.hexdigest() == data:
                 session['logged_in']=True
                 session['username']=username
-
                 flash('You are now logged in')
                 print('You are logged in')
+                return('Login Successful!')
             else:
-                error="Invalid credentials"
-
+                return("Invalid credentials!")
     else:
         return render_template('login.html')
 
